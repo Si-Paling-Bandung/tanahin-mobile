70c7fac3b8fd08cf78db6a73b43a78b788098853
david-william19 <davidddwlliam@gmail.com>
2022-06-19 18:32:21 +0700

    semoga

--- src/screen/Profiling/index.js
+++ src/screen/Profiling/index.js
@@ -1,83 +1,144 @@
-import axios from 'axios'
-import { VStack, Text, Box, IconButton, Icon, Button, HStack, Center, Input, Stack, Progress } from 'native-base'
-import React, { useEffect, useState } from 'react'
-import { BackIcon } from '../../assets/icons'
-import { fonts } from '../../utils'
-import { getData, storeData } from '../../utils/AsyncStorage'
-import { useForm } from '../../utils/useForm'
+import axios from 'axios';
+import {
+  VStack,
+  Text,
+  Box,
+  IconButton,
+  Icon,
+  Button,
+  HStack,
+  Center,
+  Input,
+  Stack,
+  Progress,
+} from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {BackIcon} from '../../assets/icons';
+import {fonts} from '../../utils';
+import {getData, storeData} from '../../utils/AsyncStorage';
+import {useForm} from '../../utils/useForm';
 
-const Profiling = ({ navigation }) => {
-    const [form, setForm] = useForm({
-        email: '',
-        password: ''
+const Profiling = ({navigation}) => {
+  const [form, setForm] = useForm({
+    email: '',
+    password: '',
+  });;
+  const [error, setError] = useState([]);
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState(0);
+  const [status, setStatus] = useState('');
+  const [color, setColor] = useState('')
+
+  const config = {
+    Headers: {
+      Authorization: `Bearer 3|c73ujsn3CUdA8XMtqiaRdSBj9f2UUMN39kBI3x21`,
+    },
+  };
+
+  const submit = () => {
+    const data = {
+      email: form.email,
+      password: form.password,
+    }
+    axios
+      .post('https://tanahin.mfaiztriputra.id/api/login', data, config)
+      .then(res => {
+        console.log('success', res.data);;
+        storeData('users', res.data);;
+        navigation.navigate('MainApp');;
+      })
+      .catch(err => {
+        console.log('error:', err.response.data.message);
+        setError(err.response.data.message);
+      });
+  };
+
+  useEffect(() => {
+    getData('users').then(res => {
+      setData(res);
+    });
+
+    getData('profile').then(res => {
+        setValue((res.profiling / 1000000).toFixed(2))
+        if(value > 0 && value < 8){
+            setStatus('low')
+            setColor('red.400')
+        }else if (value < 20 && value > 8){
+            setStatus('medium')
+            setColor('yellow.400')
+        }else if (value > 20){
+            setStatus('high')
+            setColor('lime.500')
+        }else {
+            setStatus('unverified')
+            setColor('red.400')
+        }
+    }).catch(err => {
+        console.log(err)
     })
-    const [error, setError] = useState([]);
-    const [data, setData] = useState([])
 
-    const config = {
-        Headers: {
-            Authorization: `Bearer 3|c73ujsn3CUdA8XMtqiaRdSBj9f2UUMN39kBI3x21`
-        }
-    }
+  }, []);
+  return (
+    <VStack px={'25px'} pt={'30px'} bg={'white'} h={'full'}>
+      {/* bagian header authentication */}
+      <Box alignItems="center" mb={'25px'}>
+        <IconButton
+          icon={<Icon as={<BackIcon />} />}
+          borderRadius="full"
+          bg={'#E9FAD8'}
+          mr={'auto'}
+          p={'2'}
+          onPress={() => navigation.navigate('Profile')}
+        />
+      </Box>
+      <Text
+        color={'gray.400'}
+        mr={1}
+        style={{fontFamily: fonts.primary.normal}}>
+        Hello, {data?.data_user?.name}
+      </Text>
+      <Text fontSize={'18px'} fontFamily={'heading'} fontWeight={'semibold'}>
+        Your Credit Summary
+      </Text>
 
-    const submit = () => {
-        const data = {
-            email: form.email,
-            password: form.password
-        }
-        axios.post('https://tanahin.mfaiztriputra.id/api/login', data, config).then(res => {
-            console.log('success', res.data)
-            storeData('users', res.data)
-            navigation.navigate('MainApp')
-        }).catch(err => {
-            console.log('error:', err.response.data.message)
-            setError(err.response.data.message)
-        })
-    }
-
-    useEffect(() => {
-        getData('users').then(res => {
-            setData(res)
-        })
-    }, [])
-    return (
-        <VStack px={"25px"} pt={"30px"} bg={"white"} h={"full"}>
-            {/* bagian header authentication */}
-            <Box alignItems="center" mb={'25px'}>
-                <IconButton icon={<Icon as={<BackIcon />} />}
-                    borderRadius="full"
-                    bg={"#E9FAD8"}
-                    mr={"auto"}
-                    p={'2'}
-                    onPress={() => navigation.navigate('Profile')}
-                />
-            </Box>
-            <Text
-                color={"gray.400"}
-                mr={1}
-                style={{ fontFamily: fonts.primary.normal }}>
-                Hello, {data?.data_user?.name}
+      {/* bagian circular progress */}
+      <VStack>
+        <Text textAlign={'center'} fontSize={'50px'} fontWeight={'semibold'}>
+          {value}
+        </Text>
+        <Box w="100%">
+          <Progress
+            value={value}
+            size={'lg'}
+            bg="coolGray.100"
+            _filledTrack={{
+              bg: `${color}`,
+            }}
+            max={20}
+          />
+        </Box>
+        <HStack justifyContent={'space-between'} mt={'10px'}>
+            <Text>0</Text>
+            <Text>
+                {
+                    status
+                }
             </Text>
-            <Text fontSize={"18px"} fontFamily={'heading'} fontWeight={'semibold'}>Your Credit Summary</Text>
-
-            {/* bagian circular progress */}
-            <VStack>
-                <Text textAlign={'center'} fontSize={'50px'} fontWeight={'semibold'}>0</Text>
-                <Box w="100%">
-                    <Progress
-                        value={0}
-                        size={'lg'}
-                        bg="coolGray.100"
-                        _filledTrack={{
-                            bg: "lime.600"
-                        }}
-                    />
-                </Box>
-            </VStack>
-            {/* bagian akhir circular */}
-            <Button bg="#4FAD39" mt={'auto'} mb={'80px'} colorScheme={'success'} fontWeight={"semibold"} onPress={() => navigation.navigate('ProfilingData')}>Update</Button>
-        </VStack>
-    )
-}
+            <Text>20</Text>
+        </HStack>
+      </VStack>
+      {/* bagian akhir circular */}
+      <Button
+        bg="#4FAD39"
+        mt={'auto'}
+        mb={'80px'}
+        colorScheme={'success'}
+        fontWeight={'semibold'}
+        onPress={() => navigation.navigate('ProfilingData')}>
+        Update
+      </Button>
+    </VStack>
+  );;
+};
 
 export default Profiling
--- src/screen/ProfilingData/index.js
+++ src/screen/ProfilingData/index.js
@@ -1,73 +1,113 @@
-import axios from 'axios'
-import { VStack, Text, Box, IconButton, Icon, Button, HStack, Center, Input, Stack, Progress } from 'native-base'
-import React, { useEffect, useState } from 'react'
-import { BackIcon } from '../../assets/icons'
-import { fonts } from '../../utils'
-import { getData, storeData } from '../../utils/AsyncStorage'
-import { useForm } from '../../utils/useForm'
+import axios from 'axios';
+import {
+  VStack,
+  Text,
+  Box,
+  IconButton,
+  Icon,
+  Button,
+  HStack,
+  Center,
+  Input,
+  Stack,
+  Progress,
+} from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {BackIcon} from '../../assets/icons';
+import {fonts} from '../../utils';
+import {getData, storeData} from '../../utils/AsyncStorage';
+import {useForm} from '../../utils/useForm';
 
-const ProfilingData = ({ navigation }) => {
-    const [form, setForm] = useForm({
-        phone_number: '',
-    })
-    const [error, setError] = useState([]);
-    const [data, setData] = useState([])
-    const [token, setToken] = useState('')
+const ProfilingData = ({navigation}) => {
+  const [form, setForm] = useForm({
+    phone_number: '',
+  });
+  const [error, setError] = useState([]);
+  const [data, setData] = useState([]);
+  const [token, setToken] = useState('');
 
-    const config = {
-        Headers: {
-            Authorization: `Bearer ${data?.access_token}`
-        }
-    }
+  const config = {
+    Headers: {
+      Authorization: `Bearer ${data?.access_token}`,
+    },
+  };
 
-    const submit = () => {
-        const data = {
-            phone_number: form.phone_number,
-        }
-        getData('users').then(res => {
-            axios.post('https://tanahin.mfaiztriputra.id/api/brick', data, {headers: {Authorization: `Bearer ${res.access_token}`}}).then(res => {
-                console.log('success', res.data)
-                // storeData('users', res.data)
-                storeData('dataOtp', res.data.data)
-                navigation.navigate('ProfilingToken', res.data.data)
-            }).catch(err => {
-                console.log('error:', err.response.data.message)
-                // setError(err.response.data.message)
-            })  
+  const submit = () => {
+    const data = {
+      phone_number: form.phone_number,
+    };
+    getData('users').then(res => {
+      axios
+        .post('https://tanahin.mfaiztriputra.id/api/brick', data, {
+          headers: {Authorization: `Bearer ${res.access_token}`},
         })
-    }
-    return (
-        <VStack px={"25px"} pt={"30px"} bg={"white"} h={"full"}>
-            {/* bagian header authentication */}
-            <Box alignItems="center" mb={'25px'}>
-                <IconButton icon={<Icon as={<BackIcon />} />}
-                    borderRadius="full"
-                    bg={"#E9FAD8"}
-                    mr={"auto"}
-                    p={'2'}
-                    onPress={() => navigation.navigate('Profile')}
-                />
-            </Box>
-            <Text fontSize={"18px"} fontFamily={'heading'} textAlign={"center"} mb={'20px'} fontWeight={'semibold'}>Verification</Text>
-            <Text
-                color={"gray.400"}
-                mr={1}
-                textAlign={'center'}
-                style={{ fontFamily: fonts.primary.normal }}>
-                Enter you Phone Number to
-                Get Verification of Credit Number
-            </Text>
+        .then(res => {
+          console.log('success', res.data);
+          // storeData('users', res.data)
+          storeData('dataOtp', res.data.data);
+          navigation.navigate('ProfilingToken', res.data.data);
+        })
+        .catch(err => {
+          console.log('error:', err.response.data.message);
+          // setError(err.response.data.message)
+        });
+    });
+  };
+  return (
+    <VStack px={'25px'} pt={'30px'} bg={'white'} h={'full'}>
+      {/* bagian header authentication */}
+      <Box alignItems="center" mb={'25px'}>
+        <IconButton
+          icon={<Icon as={<BackIcon />} />}
+          borderRadius="full"
+          bg={'#E9FAD8'}
+          mr={'auto'}
+          p={'2'}
+          onPress={() => navigation.navigate('Profile')}
+        />
+      </Box>
+      <Text
+        fontSize={'18px'}
+        fontFamily={'heading'}
+        textAlign={'center'}
+        mb={'20px'}
+        fontWeight={'semibold'}>
+        Verification
+      </Text>
+      <Text
+        color={'gray.400'}
+        mr={1}
+        textAlign={'center'}
+        style={{fontFamily: fonts.primary.normal}}>
+        Enter you Phone Number to Get Verification of Credit Number
+      </Text>
 
-            {/* bagian circular progress */}
-            <Stack mt={"20px"} mb={"15px"}>
-                <Text style={{ fontFamily: fonts.primary.normal }} fontSize={"14px"}>Phone Number</Text>
-                <Input placeholder='Enter your Phone Number' variant={'filled'} mt={'10px'} value={form.phone_number} onChangeText={value => setForm('phone_number', value)}></Input>
-                {error && <Text color={'red.400'}>{error.email}</Text>}
-            </Stack>
-            {/* bagian akhir circular */}
-            <Button bg="#4FAD39" mt={'auto'} mb={'80px'} colorScheme={'success'} fontWeight={"semibold"} onPress={submit}>Verify Me</Button>
-        </VStack>
-    )
-}
+      {/* bagian circular progress */}
+      <Stack mt={'20px'} mb={'15px'}>
+        <Text style={{fontFamily: fonts.primary.normal}} fontSize={'14px'}>
+          Phone Number
+        </Text>
+        <Input
+          placeholder="Enter your Phone Number"
+          variant={'filled'}
+          mt={'10px'}
+          value={form.phone_number}
+          onChangeText={value => setForm('phone_number', value)}
+        />
+        {error && <Text color={'red.400'}>{error.email}</Text>}
+      </Stack>
+      {/* bagian akhir circular */}
+      <Button
+        bg="#4FAD39"
+        mt={'auto'}
+        mb={'80px'}
+        colorScheme={'success'}
+        fontWeight={'semibold'}
+        onPress={submit}>
+        Verify Me
+      </Button>
+    </VStack>
+  );
+};
 
-export default ProfilingData
+export default ProfilingData;
